13770f434f33f8c75162a462c89ad0ef40f0c9d6
ceejayv328 <ceejayv328@gmail.com>
2022-05-24 20:01:27 +0800

    Adjusted Navbar component with style.css. Added react-icon-small.png

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,12 @@
 import react from "react";
+import reactIconSmall from "../images/react-icon-small.png"
 
 export default function Navbar() {
-    return <h1>Navbar Component</h1>
+    return (
+        <nav>
+            <img src={reactIconSmall} />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import react from "react";
 import reactDom from "react-dom"
+import "./style.css"
 import App from "./App"
 
 reactDom.render(<App />, document.getElementById("root"))
