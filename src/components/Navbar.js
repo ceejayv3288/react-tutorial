800064be98d271a3b1e5cdc65ab783a3389cbb4f
ceejayv328 <ceejayv328@gmail.com>
2022-05-30 15:38:46 +0800

    Adjust css file for classnames. Add classnames in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import reactIconSmall from "../images/react-icon-small.png"
 export default function Navbar() {
     return (
         <nav>
-            <img src={reactIconSmall} />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img className="nav--icon" src={reactIconSmall} />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
