7cf81419a2843c88c557db5b6edfce037f3929ec
ceejayv328 <ceejayv328@gmail.com>
2022-05-30 15:55:45 +0800

    Styled Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
 import react from "react";
 
 export default function Main() {
-    return <h1>Main Component</h1>
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first release in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has well over 100k stars on GitHub.</li>
+                <li>Is maintained by Meta(Previously called Facebook).</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
+            </ul>
+        </main>
+    )
 }
